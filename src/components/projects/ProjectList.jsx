e3d02c3079a884ad1f5d6f258ad18535e2e4750c
Pär Ericson <par.ericson@frontwalker.se>
2019-04-30 14:32:36 +0200

    first commit

---
+++ src/components/projects/ProjectList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProjectSummary from './ProjectSummary'
+
+const ProjectList = () => {
+  return (
+    <div className="project-listenerCount section">
+      <ProjectSummary/>
+    </div>
+  )
+}
+
+export default ProjectList;

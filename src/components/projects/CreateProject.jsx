8843d7c17badf78ab38c69d9e8759a520f7c9f31
paera <ericson.par@gmail.com>
2019-05-01 20:03:48 +0200

    added thunk and actioncreators

--- src/components/projects/CreateProject.jsx
+++ src/components/projects/CreateProject.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { createProject } from '../../store/actions/projectActions' 
+import { connect } from 'react-redux'
 
 class CreateProject extends Component {
   state = {
@@ -41,4 +43,11 @@ class CreateProject extends Component {
   }
 }
 
-export default CreateProject
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createProject: (project) => dispatch(createProject(project))
+  }
+}
+
+
+export default connect(null, mapDispatchToProps)(CreateProject)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
 import rootReducer from './store/reducers/rootReducer';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
